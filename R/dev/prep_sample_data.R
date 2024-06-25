ad245a6bdde22a0701c4ea4606ad75b40735c50d
ftom <f.tomoyuki@okayama-u.ac.jp>
2024-06-25 13:35:33 +0900

    update vignette

--- R/dev/prep_sample_data.R
+++ R/dev/prep_sample_data.R
@@ -37,9 +37,9 @@ qtl <- sample(x = df$marker_info$id, size = 3)
 qtl_index <- which(df$marker_info$id %in% qtl)
 df$marker_info[qtl_index, ]
 # id chr      pos ref_allele alt_allele
-# 3456    4_9644821   4  9644821          G          A
-# 3871   4_19751697   4 19751697          G          A
-# 11666 12_21496434  12 21496434          G          A
+# 8128    9_4426166   9  4426166          G          A
+# 9060   10_1774653  10  1774653          G          A
+# 10484 11_14150576  11 14150576          G          A
 
 qtl_ds <- df$dosage[, qtl_index]
 
@@ -50,6 +50,11 @@ q2_add <- qtl_ds[, 2] * 3
 q2_dom <- as.numeric(qtl_ds[, 2] == 1) * 0
 q3_add <- qtl_ds[, 3] * 2
 q3_dom <- as.numeric(qtl_ds[, 3] == 1) * 1
+
+st <- 1^2 + 5^2 + 3^2 + 2^2
+5^2/st
+3^2/st
+2^2/st
 
 pheno <- q1_add + q1_dom + q2_add + q2_dom + q3_add + q3_dom + e
 pheno <- data.frame(id = df$sample_info$id, pheno = pheno)
